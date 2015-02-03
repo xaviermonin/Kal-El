d3b1409fcdabf2b11a17a028b9405292be1be3d6
Xavier Monin <xavier.monin@free.fr>
2015-02-03 23:16:22 +0100

    Flags enum move from cpp to header
    KEInternetExplorer becomes a wrapper, navigate methods becomes public,
    so enums flags for navigate method move to header.

--- Client/KEBrowser/keinternetexplorer.h
+++ Client/KEBrowser/keinternetexplorer.h
@@ -9,6 +9,26 @@ class QNetworkRequest;
 class QVariant;
 struct IDispatch;
 
+typedef enum BrowserNavConstants {
+    navOpenInNewWindow = 0x1,
+    navNoHistory = 0x2,
+    navNoReadFromCache = 0x4,
+    navNoWriteToCache = 0x8,
+    navAllowAutosearch = 0x10,
+    navBrowserBar = 0x20,
+    navHyperlink = 0x40,
+    navEnforceRestricted = 0x80,
+    navNewWindowsManaged = 0x0100,
+    navUntrustedForDownload = 0x0200,
+    navTrustedForActiveX = 0x0400,
+    navOpenInNewTab = 0x0800,
+    navOpenInBackgroundTab = 0x1000,
+    navKeepWordWheelText = 0x2000,
+    navVirtualTab = 0x4000,
+    navBlockRedirectsXDomain = 0x8000,
+    navOpenNewForegroundTab = 0x10000
+} BrowserNavConstants;
+
 class KEInternetExplorer : public QObject
 {
     Q_OBJECT
