0101e9380e8f6e3e276b74604f98b44749f79302
Xavier Monin <xavier.monin@free.fr>
2015-02-08 00:39:12 +0100

    Add post unit test

--- Client/Tests/KENetwork/tst_keinternetexplorertest.cpp
+++ Client/Tests/KENetwork/tst_keinternetexplorertest.cpp
@@ -14,9 +14,10 @@ private Q_SLOTS:
     void initTestCase();
     void cleanupTestCase();
     void get();
+    void post();
 
 private:
-    KEInternetExplorer* ie;
+
 };
 
 KEInternetExplorerTest::KEInternetExplorerTest()
@@ -25,32 +26,47 @@ KEInternetExplorerTest::KEInternetExplorerTest()
 
 void KEInternetExplorerTest::initTestCase()
 {
-    ie = new KEInternetExplorer(false);
+
 }
 
 void KEInternetExplorerTest::cleanupTestCase()
 {
-    if (ie != NULL)
-    {
-        delete ie;
-        ie = NULL;
-    }
 }
 
 void KEInternetExplorerTest::get()
 {
-    QSignalSpy spy(ie, SIGNAL(navigateComplete(QString)));
+    KEInternetExplorer ie(false);
 
-    ie->navigate("http://www.perdu.com/");
+    QSignalSpy spy(&ie, SIGNAL(navigateComplete(QString)));
+
+    ie.navigate("http://www.perdu.com/");
 
     while (spy.count() == 0)
         QTest::qWait(100);
 
-    QString contentHtml = ie->contentHtml();
-    QString contentText = ie->contentText();
+    QString contentHtml = ie.contentHtml();
+    QString contentText = ie.contentText();
 
     QVERIFY(contentHtml.contains("<title>Vous Etes Perdu ?</title>"));
     QVERIFY(contentText.contains("Perdu sur l'Internet ?\r\n"));
+}
+
+void KEInternetExplorerTest::post()
+{
+    KEInternetExplorer ie(false);
+
+    QSignalSpy spy(&ie, SIGNAL(navigateComplete(QString)));
+
+    ie.navigate("http://www.htmlcodetutorial.com/cgi-bin/mycgi.pl",
+                 "Content-Type: application/x-www-form-urlencoded",
+                 "kepropertyname=kepropertyvalue");
+
+    while (spy.count() == 0)
+        QTest::qWait(100);
+
+    QString contentText = ie.contentText();
+
+    QVERIFY(contentText.contains("kepropertyname"));
 }
 
 QTEST_MAIN(KEInternetExplorerTest)
